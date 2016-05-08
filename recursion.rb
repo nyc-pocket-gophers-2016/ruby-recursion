8279992a8cbaa0fee5d78a0f598e762fb55f5b6d
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-05-08 09:24:02 -0400

    Write two recursive functions for palindromes and bottles of beer countdown

--- recursion.rb
+++ recursion.rb
@@ -1,8 +1,26 @@
 def is_palindrome?(string)
 
-	#bc is at mid
+	return true if string.length == 0
+	
+	if string[0] == string[-1]
+		string.delete!(string[0])
+		is_palindrome?(string)
+	else
+		return false
+	end
 end
 
 def bottles_of_beer_on_wall(n)
 
+	return n if n == 0
+	
+	puts "#{n} bottles of beer on the wall!"
+	bottles_of_beer_on_wall(n-1)
+
+end
+
+def recursive_flatten(arr)
+
+	
+	
 end
