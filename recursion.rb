60180b86d990b872b57b4082939cfcb931a471ee
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-05-08 15:17:29 -0400

    Create 3 recursive functions: is_palindrome?, bottles_of_beer_on_wall, the_recursion (used in tandem with recursive_flatten)

--- recursion.rb
+++ recursion.rb
@@ -20,7 +20,24 @@ def bottles_of_beer_on_wall(n)
 end
 
 def recursive_flatten(arr)
+	
+	i = 0
+	
+	the_recursion(arr, i)
+	
+end
 
+def the_recursion(arr, i)
 	
-	
+	if arr.any? {|val| val.class == Array}
+		if arr[i].class == Array
+			arr.flatten!
+			i += 1	
+			the_recursion(arr, i)
+		else 
+			arr[i]
+		end
+	else
+		return arr
+	end
 end
